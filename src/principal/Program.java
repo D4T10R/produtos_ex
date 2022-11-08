e45088dc28727656f2627510105cf4b24e18b639
Datior <leonardowederveiga@yahoo.com>
2022-11-08 18:16:05 -0300

    Adiantamento na class principal

--- src/principal/Program.java
+++ src/principal/Program.java
@@ -1,14 +1,47 @@
 package principal;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+import javax.lang.model.util.ElementScanner14;
+
 public class Program {
     
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         
-        // ATRIBUTOS
+        Scanner sc = new Scanner(System.in);
 
-        // ATRIBUTOS
+        try {
+            System.out.println("\n------SEJA BEM VINDO!------");
+            System.out.print("Quatos produtos você deseja cria? ");
+            int quantosProdutos = sc.nextInt();
+            for (int i = 0; i < quantosProdutos; i++) {
+                System.out.println("\n1 - Comida \n2 - Roupa \n3 - Objeto");
+                int tipo = sc.nextInt();
+                System.out.print("\nNome: ");
+                String nome = sc.nextLine();
+                System.out.print("preço: ");
+                Double preco = sc.nextDouble();
+                System.out.print("Quantidade: ");
+                Integer quantidade = sc.nextInt();
+                if (tipo == 1) {
 
-        
+                } else if (tipo == 2) {
+
+                } else if (tipo == 3) {
+
+                } else {
+                    System.out.println("ERRO! no tipo de produto, repita novamente");
+                    i--;
+                }
+
+            }
+        }
+
+
 
 
     }
